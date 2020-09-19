d6ef4754e971f814a263b9169c6b9557dc7b38e9
jagberg <jagberg@gmail.com>
2020-09-19 22:02:35 +1000

    fix: Move files to an unwatched location
    When files were copied here it was trying to create the folders here triggering another event to run this script. This will now copy to another location.
    
    Updated logs but its still not writing to them, likely a permission issue.
    
    Had to add a sleep of 5 seconds because incron wouldnt let me know once the entire file had been moved to this location. Ideally it would.
    
    Dont move the root path so added validation here.

--- OrganiseTorrents.sh
+++ OrganiseTorrents.sh
@@ -1,13 +1,22 @@
-
 #!/bin/bash
 # GNU bash, version 4.4.20
 defaultLogPath=/tmp/logs
+defaultOrganisedRootPath=/mnt/torrents/organised
 
 rootpath=$1
 video=$2
 logPath=${3:-$defaultLogPath}
+organisedRootPath=${4:-$defaultOrganisedRootPath}
 
-echo "Path: " $rootpath ", Video: " $video ", Log Path: " $logPath
+# Wait for the file to finish moving into the watched location. I couldnt get incron to only start the process once it completed.
+sleep 5
+
+echo "$(date)" >> $logPath/organiseTorrent.log
+echo "Path: " $rootpath ", Video: " $video ", Log Path: " $logPath ", Organised Root Path: " $organisedRootPath >> $logPath/organiseTorrent.log
+
+if [ "$rootpath" == "$video" ] || [ -z "$video" ]; then
+   exit 1
+fi
 
 # Extract the name before SxxEyy eg. Extract Hello.World from Hello.World.S01E03.xyz
 fileSuffix=$(echo "$video" | grep -oP '.+?(?=\.S\d\dE\d\d\.)')
@@ -22,11 +31,10 @@ fi
 # Replace . with space
 targetPath=${fileSuffix//./ }
 
-# TODO: Change to local timezone
-echo "$(date)" >> $logPath/organiseTorrent.log
 echo "New Folder: " $targetPath >> $logPath/organiseTorrent.log
 
-cd $rootpath
+cd $organisedRootPath
 mkdir -p "$targetPath"
 
-echo "Move From: '$rootpath/$video'" " To: '$rootpath/$targetPath/$video'" >> $logPath/organiseTorrent.logmv "$rootpath/$video" "$rootpath/$targetPath/$video"
+echo "Move From: '$rootpath/$video'" " To: '$organisedRootPath/$targetPath/$video'" >> $logPath/organiseTorrent.log
+mv "$rootpath/$video" "$organisedRootPath/$targetPath/$video"
